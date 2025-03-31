40fc4c04afb490655b48098a3293676659d8e1f8
aclementcl <alejandro.clemente.i@gmail.com>
2025-03-30 23:14:12 -0300

    Adding stripe session to create payment session

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,10 +3,19 @@ import * as joi from 'joi';
 
 interface EnvVars {
     PORT: number;
+    STRIPE_SECRET: string;
+    STRIPE_SUCCESS_URL: string;
+    STRIPE_CANCEL_URL: string;
+    STRIPE_ENDPOINT_SECRET: string;
 }
 
 const envsSchema = joi.object({
     PORT: joi.number().required(),
+    
+    STRIPE_SECRET: joi.string().required(),
+    STRIPE_SUCCESS_URL: joi.string().required(),
+    STRIPE_CANCEL_URL: joi.string().required(),
+    STRIPE_ENDPOINT_SECRET: joi.string().required(),
 }).unknown(true);
 
 const { error, value } = envsSchema.validate(process.env);
@@ -19,4 +28,8 @@ const envsVars: EnvVars = value;
 
 export const envs = {
     port: envsVars.PORT,
+    stripeSecret: envsVars.STRIPE_SECRET,
+    stripeSuccessUrl: envsVars.STRIPE_SUCCESS_URL,
+    stripeCancelUrl: envsVars.STRIPE_CANCEL_URL,
+    stripeEndpointSecret: envsVars.STRIPE_ENDPOINT_SECRET,
 }
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,4 +1,69 @@
 import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payment-session.dto';
+import { Request, Response } from 'express';
+import { envs } from 'src/config';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+    private readonly stripe = new Stripe(process.env.STRIPE_SECRET);
+
+    async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+
+        const { currency, items, orderId } = paymentSessionDto;
+
+        const lineItems = items.map(item => {
+            return {
+                price_data: {
+                    currency: currency,
+                    product_data: {
+                        name: item.name,
+                    },
+                    unit_amount: Math.round(item.price * 100), //20 usd = 20.00
+                },
+                quantity: item.quantity,
+            }
+        });
+
+        const session = await this.stripe.checkout.sessions.create({
+
+            payment_intent_data: {
+                metadata: {
+                    orderId: orderId,
+                }
+            },
+            line_items: lineItems,
+            mode: 'payment',
+            success_url: envs.stripeSuccessUrl,
+            cancel_url: envs.stripeCancelUrl,
+        });
+
+        return session;
+    }
+
+    async stripeWebhook(req: Request, res: Response) {
+        const sig = req.headers['stripe-signature'];
+
+        let event: Stripe.Event;
+        const endpointSecret = envs.stripeEndpointSecret;
+        try {
+            event = this.stripe.webhooks.constructEvent(req['rawBody'], sig, endpointSecret);
+        } catch (err) {
+            return res.status(400).send(`Webhook Error: ${err.message}`);
+        }
+
+        switch (event.type) {
+            case 'charge.succeeded':
+                const chargeSucceeded = event.data.object;
+                console.log({
+                    metadata: chargeSucceeded.metadata,
+                    orderId: chargeSucceeded.metadata.orderId,
+                });
+                break;
+            default:
+                console.log(`Unhandled event type ${event.type}`);
+        }
+
+        return res.status(200).json({ sig });
+    }
+}
