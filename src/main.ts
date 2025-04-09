351bce56832e153ae3f79320571594b68eef4c85
aclementcl <alejandro.clemente.i@gmail.com>
2025-04-09 17:23:43 -0400

    Adding nats

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, RawBody, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const logger = new Logger('Payments');
+  const logger = new Logger('Payments MS');
 
   const app = await NestFactory.create(AppModule, {
     rawBody: true,
@@ -14,6 +15,17 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: envs.natsServers,
+    }
+  }, {
+    inheritAppConfig: true,
+  });
+
+  await app.startAllMicroservices();
 
   await app.listen(envs.port);
 
